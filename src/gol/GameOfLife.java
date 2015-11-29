ea6cb4341f07db62c2a5a73d18825e02c0f90d3a
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2015-11-29 14:50:48 +0100

    Extract History class

--- src/gol/GameOfLife.java
+++ src/gol/GameOfLife.java
@@ -38,39 +38,6 @@ public class GameOfLife {
 		return n;
 	}
 
-	static class History {
-		List<String> world;
-		int heightOffset;
-		int widthOffset;
-
-		History(List<String> world, int heightOffset, int widthOffset) {
-			this.world = world;
-			this.heightOffset = heightOffset;
-			this.widthOffset = widthOffset;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			History other = (History) obj;
-			if (heightOffset != other.heightOffset)
-				return false;
-			if (widthOffset != other.widthOffset)
-				return false;
-			if (world == null) {
-				if (other.world != null)
-					return false;
-			} else if (!world.equals(other.world))
-				return false;
-			return true;
-		}
-	}
-
 	private int steps = -1;
 	private List<String> world = null;
 	private int height = -1;
