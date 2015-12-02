e9cd17b42812b89baa7b301026ba8534d91a066d
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2015-12-02 22:26:26 +0100

    Extract long boolean expressions in simulation-loop

--- src/gol/Simulation.java
+++ src/gol/Simulation.java
@@ -17,16 +17,24 @@ public class Simulation {
 		OptionalInt loop = OptionalInt.empty();
 		World world = conf.world;
 
-		for (int step = 0; step <= conf.stepLimit && !loop.isPresent(); ++step) {
+		for (int step = 0; doIterate(conf, loop, step); ++step) {
 			if (step != 0)
 				world = world.nextWorld();
 
 			loop = conf.loopDetector.addSimulationStepAndDetect(world);
 
-			if (!conf.quietMode || step == conf.stepLimit || loop.isPresent())
+			if (doPrintStep(conf, loop, step))
 				conf.stepPrinter.printStep(world, step, loop);
 
 			conf.periodicBlocker.blockRestOfPeriodAndRestart();
 		}
+	}
+
+	private boolean doIterate(SimulationConfig conf, OptionalInt loop, int step) {
+		return step <= conf.stepLimit && !loop.isPresent();
+	}
+
+	private boolean doPrintStep(SimulationConfig conf, OptionalInt loop, int step) {
+		return !conf.quietMode || step == conf.stepLimit || loop.isPresent();
 	}
 }
