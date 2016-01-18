9e9c215ed66a99678029b1a5cb282c9f97738f91
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2016-01-18 21:23:34 +0100

    Add World hashCode and equals decorator

--- src/gol/world/World.java
+++ src/gol/world/World.java
@@ -15,9 +15,86 @@ public interface World extends Iterable<Cell> {
 
 	public abstract void setAlive(Cell cell);
 
+	@Deprecated
 	public abstract Set<Cell> getAliveCells();
 
 	public static World create() {
-		return new CoordBoolMapWorld();
+		return withHashAndEquals(new CoordBoolMapWorld());
+	}
+
+	public static World withHashAndEquals(World world) {
+		return new World() {
+
+			Integer hc = null;
+
+			@Override
+			public Iterator<Cell> iterator() {
+				return world.iterator();
+			}
+
+			@Override
+			public boolean isAlive(int x, int y) {
+				return world.isAlive(x, y);
+			}
+
+			@Override
+			public boolean isAlive(Cell cell) {
+				return world.isAlive(cell);
+			}
+
+			@Override
+			public void setAlive(int x, int y) {
+				world.setAlive(x, y);
+				hc = null;
+			}
+
+			@Override
+			public void setAlive(Cell cell) {
+				world.setAlive(cell);
+				hc = null;
+			}
+
+			@Override
+			public Set<Cell> getAliveCells() {
+				return world.getAliveCells();
+			}
+
+			@Override
+			public int hashCode() {
+				if (hc == null) {
+					hc = 0;
+					for (Cell c : world) {
+						hc += c.hashCode();
+					}
+				}
+
+				return hc;
+			}
+
+			@Override
+			public boolean equals(Object other) {
+				if (this == other)
+					return true;
+				if (other == null)
+					return false;
+				if (this.hashCode() != other.hashCode())
+					return false;
+				if (!(other instanceof World))
+					return false;
+
+				World otherWorld = (World) other;
+
+				for (Cell c : this) {
+					if (!otherWorld.isAlive(c))
+						return false;
+				}
+
+				for (Cell c : otherWorld) {
+					if (!this.isAlive(c))
+						return false;
+				}
+				return true;
+			}
+		};
 	}
 }
