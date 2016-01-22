1ee66291c3287e67588adb1d864d1653c6a16586
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2016-01-23 00:53:34 +0100

    Replace string concatenation with StringBuilder

--- src/gol/StepPrinter.java
+++ src/gol/StepPrinter.java
@@ -1,22 +1,23 @@
 package gol;
 
-import java.util.OptionalInt;
-
 import gol.world.World;
+
+import java.util.OptionalInt;
 
 public interface StepPrinter {
 	void printStep(World world, int stepCount, OptionalInt loop);
-	
-	static StepPrinter fixedViewPort(int width, int height, OutputFormat cellFormat) {
+
+	static StepPrinter fixedViewPort(int width, int height,
+			OutputFormat cellFormat) {
 		return new StepPrinter() {
 
 			@Override
-			public void printStep(World world,int stepCount, OptionalInt loop) {
+			public void printStep(World world, int stepCount, OptionalInt loop) {
 				for (int y = 0; y < height; ++y) {
-					String line = "";
-					for (int x = 0; x < width; ++x) 
-						line += cellFormat.cell(world.isAlive(x, y));
-					System.out.println(line);
+					StringBuilder sb = new StringBuilder();
+					for (int x = 0; x < width; ++x)
+						sb.append(cellFormat.cell(world.isAlive(x, y)));
+					System.out.println(sb.toString());
 				}
 
 				System.out.println(getTitle(stepCount, loop));
@@ -27,8 +28,9 @@ public interface StepPrinter {
 				if (stepCount == 0)
 					return "start";
 				else if (loop.isPresent())
-					return String.format("step %s - loop of length %s detected",
-							stepCount, loop.getAsInt());
+					return String.format(
+							"step %s - loop of length %s detected", stepCount,
+							loop.getAsInt());
 				else
 					return "step " + stepCount;
 			}
