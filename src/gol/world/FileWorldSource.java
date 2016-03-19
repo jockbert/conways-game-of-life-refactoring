7ac667ab61c851557097be48d30a3e4d7cbc327b
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2016-03-19 01:01:17 +0100

    Improve file read performance

---
+++ src/gol/world/FileWorldSource.java
@@ -0,0 +1,84 @@
+package gol.world;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FileWorldSource implements WorldSource {
+
+	private String filePath;
+
+	public FileWorldSource(String filePath) {
+		this.filePath = filePath;
+	}
+
+	@Override
+	public WorldSourceResult generate() {
+
+		InputStream is = openStream();
+
+		int y = 0;
+		int x = 0;
+		int maxWidth = 0;
+
+		World world = World.create();
+
+		try {
+			for (int i = is.read(); i != -1; i = is.read()) {
+				char c = (char) i;
+				switch (c) {
+				case '#':
+					world.setAlive(x, y);
+				case '-':
+					x++;
+					break;
+				case '\n':
+					maxWidth = Math.max(maxWidth, x);
+					y++;
+					x = 0;
+				case '\r':
+					break;
+
+				default:
+					throwInvalidCharacter((char) i, y + 1);
+				}
+			}
+			maxWidth = Math.max(maxWidth, x);
+			is.close();
+		} catch (IOException e) {
+			// silence
+		}
+
+		int height = y + (x != 0 ? 1 : 0);
+		return WorldSource.result(world, maxWidth, height);
+	}
+
+	private InputStream openStream() {
+		File source = new File(filePath);
+		if (!source.exists())
+			doThrow(filePath + " (No such file or directory)");
+		else if (source.isDirectory())
+			doThrow(filePath + " (Is a directory)");
+		InputStream is = null;
+		try {
+			is = new BufferedInputStream(new FileInputStream(source));
+		} catch (FileNotFoundException e) {
+			doThrow(e.getMessage());
+		}
+
+		return is;
+	}
+
+	private void throwInvalidCharacter(char c, int lineNumber) {
+		String format = "Invalid character '%s' on line %s in file %s";
+		String message = String.format(format, c, lineNumber, filePath);
+		doThrow(message);
+	}
+
+	private void doThrow(String msg) {
+		throw new RuntimeException(msg);
+	}
+}
